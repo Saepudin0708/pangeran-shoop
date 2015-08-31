d0cf794f73c951d4c6ac31929fcc07453c830143
Saepudin <saepudin.0708@email.unikom.ac.id>
2015-08-31 20:27:19 +0700

    create fitur verification email

--- register.php
+++ register.php
@@ -5,11 +5,11 @@ include "src/User.php";
 $error = "";
 $User = new User($dbh);
 
+
 if (isset($_POST['submit'])) {
 	
 	/*
 	 * Cek form pendaftaran
-	 * Array ( [name] => [email] => [password] => [password_conf] => [address] => [city] => [phone] => [submit] => ) 
 	 */	 
 	if (empty($_POST['name'])) {
 		$error .= "<p>Nama harus diisi</p>";
@@ -19,7 +19,7 @@ if (isset($_POST['submit'])) {
 		$error .= "<p>Email harus diisi</p>";
 	}
 	
-	if (empty($_POST['password']) || $_POST['password'] != $_POST['password_conf']) {
+	if (empty($_POST['password'] || $_POST['password'] != $_POST['password_conf'])) {
 		$error .= "<p>Password tidak valid</p>";
 	}
 	
@@ -35,13 +35,31 @@ if (isset($_POST['submit'])) {
 			$_POST['gender'],
 			$_POST['phone']
 		);
-		
-		echo "
-			<script>
-				window.location = 'register.php';
-			</script>
-		";
 	}
+		if ($User) {
+
+			
+			$to = $_POST['email'];
+
+			$subject = "Activate Your Email";
+
+			$header = "From: Saepudin saepudin.0708@gmail.com";
+
+			$message = "Please click the link below to verify and activate your account. \r\n";
+			$message .= "http://www.yourweb.com/confirm.php";
+
+			$sentmail = mail($to, $subject, $message, $header);
+		} else {
+			echo "Email tidak valid";
+		}
+		print_r($sentmail);
+
+		if ($sentmail) {
+			echo "Your Confirmation Link Has Been Sent to Your Email Address.";
+		} else {
+			echo "Cannot Send Confirmation Link to Your Email Address";
+		}
+	
 }
 
 include "view/register.php";
